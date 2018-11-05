fdb0f09c5f61f6568f92442dcb3ffd5904995bf6
Ahmed <ahmadmzak@gmail.com>
2018-11-05 20:32:57 +0500

    Updates

---
+++ precache-manifest.d63496469122597d0aafb5ad0e7317ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ec6ec046c05724b4e46352fda3cc552",
+    "url": "/wakecap/static/media/logo.4ec6ec04.png"
+  },
+  {
+    "revision": "ac24e064ad04837f8c07",
+    "url": "/wakecap/static/js/runtime~main.ac24e064.js"
+  },
+  {
+    "revision": "dec1d06c382bdd8b82d7",
+    "url": "/wakecap/static/js/main.dec1d06c.chunk.js"
+  },
+  {
+    "revision": "e305ea0216656ed950d9",
+    "url": "/wakecap/static/js/1.e305ea02.chunk.js"
+  },
+  {
+    "revision": "018eb3ba655cc2842324deb48caebeec",
+    "url": "/wakecap/index.html"
+  }
+];
